7cb30bd93636e6ebfbb44b04b6087fc0b4c61cfa
陈子昂 <a960015420@163.com>
2024-04-17 12:36:00 +0800

    init

--- src/test/java/org/example/mahjong/MahjongApplicationTests.java
+++ src/test/java/org/example/mahjong/MahjongApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MahjongApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
