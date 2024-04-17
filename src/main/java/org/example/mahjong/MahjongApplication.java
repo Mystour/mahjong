b96a2ff56c4adccdbdecd5b43ef8693c1f108329
Leo <960015420@qq.com>
2024-04-17 12:35:51 +0800

    Initial commit

---
+++ src/main/java/org/example/mahjong/MahjongApplication.java
@@ -0,0 +1,13 @@
+package org.example.mahjong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahjongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahjongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/mahjong/MahjongApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mahjong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahjongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
