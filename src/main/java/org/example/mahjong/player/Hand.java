3ef51828e0fcc64b6a1c180e21ff80f515eab5bd
SunLingrui <143977561+SunLingrui@users.noreply.github.com>
2024-04-21 23:01:47 +0800

    check winning

--- src/main/java/org/example/mahjong/player/Hand.java
+++ src/main/java/org/example/mahjong/player/Hand.java
@@ -1,13 +1,10 @@
 package org.example.mahjong.player;
-
-import org.example.mahjong.tile.Tile;
-
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import org.example.mahjong.tile.*;
 
 public class Hand {
     public List<Tile>[] handcard;
+    public int count;
 
     public Hand() {
         handcard = new List[5];
@@ -53,7 +50,74 @@ public class Hand {
             sortCard(i);
         }
     }
+
+
+
+
+
+
     public boolean isValidMahjong() {
-        return false;
+        int pair = 0;
+        int triple = 0;
+        int others = 0;
+        for(int i = 0; i < handcard.length; i++){
+            List<Tile> TileType = handcard[i];
+            int index = 0;
+            while (index < handcard[i].size()) {
+                Tile currentTile = handcard[i].get(index);
+
+                if (index + 1 < handcard[i].size()) {
+                    Tile nextTile = handcard[i].get(index + 1);
+
+                    if (index + 2 < handcard[i].size()) {
+                        Tile nextNextTile = handcard[i].get(index + 2);
+                        if (isPair(currentTile, nextTile)) {
+                            pair++;
+                            index += 2; // 跳过对子
+                        } else if (isTriplet(currentTile, nextTile, nextNextTile)) {
+                            triple++;
+                            index += 3; // 跳过刻子
+                        } else if (isSequence(currentTile, nextTile, nextNextTile)) {
+                            triple++;
+                            index += 3; // 跳过顺子
+                        } else {
+                            others++;
+                            index++;
+                        }
+                    } else {
+                        if (isPair(currentTile, nextTile)) {
+                            pair++;
+                            System.out.println("对子：" + currentTile + ", " + nextTile);
+                        } else {
+                            others += 2;
+                        }
+                        index += 2;
+                    }
+                } else {
+                    others++;
+                    index++;
+                }
+            }
+        }
+        if(pair == 7){
+            return true;
+        } else if (pair == 1 && triple == 4) {
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+    public static boolean isPair(Tile tile1, Tile tile2) {
+        return tile1.compareTo(tile2) == 0;
+    }
+
+    public static boolean isTriplet(Tile tile1, Tile tile2, Tile tile3) {
+        return tile1.compareTo(tile2) == 0 && tile2.compareTo(tile3) == 0;
+    }
+    // this part a bit uncertain
+    public static boolean isSequence(Tile tile1, Tile tile2, Tile tile3) {
+        return tile1.compareTo(tile2) == -1 && tile2.compareTo(tile3) == -1;
+
     }
 }
