466b8e4352f1a5f23fcaf9f89fabb3c923aeb93a
SunLingrui <143977561+SunLingrui@users.noreply.github.com>
2024-04-22 14:30:22 +0800

    add some comment of the method of the isvaildmahjong

--- src/main/java/org/example/mahjong/player/Hand.java
+++ src/main/java/org/example/mahjong/player/Hand.java
@@ -4,7 +4,7 @@ import org.example.mahjong.tile.*;
 
 public class Hand {
     public List<Tile>[] handcard;
-    public int count;
+    public int count; // 明牌区的计数器，负责计杠，碰，吃的次数
 
     public Hand() {
         handcard = new List[5];
@@ -60,10 +60,9 @@ public class Hand {
         int pair = 0;
         int triple = 0;
         int others = 0;
-        for(int i = 0; i < handcard.length; i++){
-            List<Tile> TileType = handcard[i];
+        for(int i = 0; i < handcard.length; i++){ //遍历五种类型的list
             int index = 0;
-            while (index < handcard[i].size()) {
+            while (index < handcard[i].size()) { //遍历每个list里的tile
                 Tile currentTile = handcard[i].get(index);
 
                 if (index + 1 < handcard[i].size()) {
@@ -71,13 +70,13 @@ public class Hand {
 
                     if (index + 2 < handcard[i].size()) {
                         Tile nextNextTile = handcard[i].get(index + 2);
-                        if (isPair(currentTile, nextTile)) {
+                        if (isPair(currentTile, nextTile)) { //看是不是对子
                             pair++;
                             index += 2; // 跳过对子
-                        } else if (isTriplet(currentTile, nextTile, nextNextTile)) {
+                        } else if (isTriplet(currentTile, nextTile, nextNextTile)) { //看是不是刻子
                             triple++;
                             index += 3; // 跳过刻子
-                        } else if (isSequence(currentTile, nextTile, nextNextTile)) {
+                        } else if (isSequence(currentTile, nextTile, nextNextTile)) { // 看是不是顺子
                             triple++;
                             index += 3; // 跳过顺子
                         } else {
@@ -85,39 +84,42 @@ public class Hand {
                             index++;
                         }
                     } else {
+                        //末尾剩两张
                         if (isPair(currentTile, nextTile)) {
                             pair++;
-                            System.out.println("对子：" + currentTile + ", " + nextTile);
                         } else {
                             others += 2;
                         }
                         index += 2;
                     }
                 } else {
+                    //末尾剩一张
                     others++;
                     index++;
                 }
             }
         }
-        if(pair == 7){
+        //判断有没有胡牌
+        if(pair == 7){ //七小对
             return true;
-        } else if (pair == 1 && triple == 4) {
+        } else if (pair == 1 && triple + count == 4) { //4个三个的，一个两个的，正常胡牌格式
             return true;
         }else{
             return false;
         }
 
     }
+    //判断是否为对子
     public static boolean isPair(Tile tile1, Tile tile2) {
         return tile1.compareTo(tile2) == 0;
     }
-
+    //判断是否为刻子
     public static boolean isTriplet(Tile tile1, Tile tile2, Tile tile3) {
         return tile1.compareTo(tile2) == 0 && tile2.compareTo(tile3) == 0;
     }
-    // this part a bit uncertain
+    //判断是否为顺子
     public static boolean isSequence(Tile tile1, Tile tile2, Tile tile3) {
-        return tile1.compareTo(tile2) == -1 && tile2.compareTo(tile3) == -1;
+        return tile2.getNumber() - tile1.getNumber() == 1 && tile3.getNumber() - tile2.getNumber() == 1;
 
     }
 }
