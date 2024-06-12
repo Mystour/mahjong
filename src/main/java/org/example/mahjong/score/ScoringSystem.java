8690a5e831a6f881e126f2a2c0c7be9761371337
SunLingrui <143977561+SunLingrui@users.noreply.github.com>
2024-06-12 18:42:48 +0800

    Update ScoringSystem.java

--- src/main/java/org/example/mahjong/score/ScoringSystem.java
+++ src/main/java/org/example/mahjong/score/ScoringSystem.java
@@ -6,7 +6,7 @@ import org.example.mahjong.tile.*;
 
 public class ScoringSystem implements Scorable{
 
-    public List<Tile>[] allCard; // 手牌+杠吃碰的牌
+    public List<Tile>[] allCard; // handcard + the chow pung kong card
     public int score;
     public Hand hand;
     public ScoringSystem(Hand hand) {
@@ -14,15 +14,15 @@ public class ScoringSystem implements Scorable{
         this.hand = hand;
         allCard = hand.allCard();
 
-        //将名牌区的牌和手牌区的合在一起，方便判断清一色和一条龙
+        //put all the card together can easily to calculate the score
     }
 
 
     public int calculateScore() {
         return score * SevenPairs() * uniformTile() * AllTriple() * OneDragon();
     }
-    public int uniformTile(){ // 检查是不是清一色
-        int count = 0; //计算有几种类型的牌在手牌里
+    public int uniformTile(){ // check whether the tile is uniform
+        int count = 0; 
         for(int i = 0; i < allCard.length; i++){
             if(!allCard[i].isEmpty()){
                 count++;
@@ -35,7 +35,7 @@ public class ScoringSystem implements Scorable{
         return 1;
     }
 
-    public int OneDragon(){ // 检查是不是一条龙
+    public int OneDragon(){ 
         for(int i = 0; i < 3; i++){
             int index = 1;
             int count = 0;
@@ -62,22 +62,22 @@ public class ScoringSystem implements Scorable{
         return tile2.getNumber() - tile1.getNumber() == 1;
     }
 
-    public int SevenPairs() { // 检查是不是七小对
+    public int SevenPairs() {
 
         int kongs = (hand.getKongs().size())/4;
 
         if (hand.getPair() == 7) {
-            return 2; // 普通七小对
+            return 2; // normal seven pair
         } else if (hand.getPair() + kongs * 2 == 7) {
-            return 4; // 带杠的七小对
+            return 4; // seven pair contains kong
         }
         return 1;
     }
 
 
 
-    public int AllTriple(){ // 检查是不是都是由刻字和对子形成的胡
-        //这个部分需要改，不好获取名牌堆里的碰的牌的数量
+    public int AllTriple(){ // 
+
         if(hand.getPair() == 1 && hand.getTriple() + hand.getPungs().size()/3== 4){
             return 2;
         }
